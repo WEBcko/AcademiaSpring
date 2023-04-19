460428f7113425dea5eb6e755ca1fae186da6799
Erick Ojeda <erickojeda2003@hotmail.com>
2023-04-19 01:20:50 -0300

    Nova Entidade e novos atributos

--- src/main/java/br/com/webcko/academia/entity/AbstractEntity.java
+++ src/main/java/br/com/webcko/academia/entity/AbstractEntity.java
@@ -2,6 +2,9 @@ package br.com.webcko.academia.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
@@ -11,6 +14,14 @@ public abstract class AbstractEntity {
         @GeneratedValue(strategy = GenerationType.AUTO)
         @Column(name = "id", nullable = false)
         private Long id;
+
+        @Getter @Setter
+        @Column(name = "dt_cadastrado", nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+        private LocalDateTime dataCadastrado;
+
+        @Getter @Setter
+        @Column(name = "dt_atualizado", nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+        private LocalDateTime dataAtualizado;
 
 
 }
