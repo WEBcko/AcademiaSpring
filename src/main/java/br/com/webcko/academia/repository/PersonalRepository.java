25a065e89d93562e424bbaf05efc065777db8ff0
emilio Anastácio de Paula Correa <emilioanastacio@gmail.com>
2023-05-03 07:57:06 -0300

    personal controller e personal repository

--- src/main/java/br/com/webcko/academia/repository/PersonalRepository.java
+++ src/main/java/br/com/webcko/academia/repository/PersonalRepository.java
@@ -1,10 +1,17 @@
 package br.com.webcko.academia.repository;
 
+import br.com.webcko.academia.entity.Cliente;
 import br.com.webcko.academia.entity.Personal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface PersonalRepository extends JpaRepository<Personal, Long> {
+
+    @Query("from Personal where ativo = true")
+    public List<Personal> findPersonalByAtivo ();
 }
